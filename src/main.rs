c5a51cf9c9593a32e90a72c5ec82235b864b8861
Martin Hafskjold Thoresen <martinhath@gmail.com>
2018-02-13 20:42:57 +0100

    Its working

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
+extern crate console;
+extern crate dialoguer;
 
 use serde_json::Value;
+use dialoguer::{Input, Select};
 
-use std::env::args;
+use std::process::{Command, Stdio};
+use std::io::Write;
 
 const API_KEY: &'static str = "AIzaSyD-LJtaJMPLkPJ6CjyZcqU9QpU-7Fw0Z_E";
 
@@ -56,16 +60,35 @@ fn get_data(body: &str) -> Vec<Video> {
     v
 }
 
+
+fn play_video(vid: &Video) {
+    let yt_link = format!("https://www.youtube.com/watch?v={}", vid.id);
+    let child = Command::new("mpv").arg(&yt_link)
+        .stdout(Stdio::piped())
+        .spawn().expect(
+        "Failed to execute command",
+    );
+    child.wait_with_output().expect("failed to wait on child");
+}
+
+const PROMPT: &str = "[search]";
+
 fn main() {
-    let search = args().skip(1).fold(String::new(), |mut s, e| {
-        s.push_str(&e);
-        s.push(' ');
-        s
-    });
-    let mut res = req(&search);
-    let body = res.text().unwrap();
-    let vids = get_data(&body);
-    for video in vids.iter() {
-        println!("[{}]", video.name);
+    let term = console::Term::stdout();
+    if let Ok(search_term) = Input::new(PROMPT).interact() {
+        let mut res = req(&search_term);
+        let body = res.text().unwrap();
+        let vids = get_data(&body);
+
+        let mut select = Select::new();
+        let strs = vids.iter()
+            .map(|v| format!("[{}]", v.name))
+            .collect::<Vec<_>>();
+        for s in &strs {
+            select.item(s);
+        }
+        if let Ok(i) = select.interact_on(&term) {
+            play_video(&vids[i]);
+        }
     }
 }
